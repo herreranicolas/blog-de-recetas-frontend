e32ce60ae08e0b13192c5279b3a0c72bf04bae47
Nicolas Herrera <herreranicoo17@gmail.com>
2023-06-20 16:39:45 -0300

    Agregue el maquetado inicial del footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "./components/common/Menu";
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
     <>
       <Menu></Menu>
+      <Footer></Footer>
     </>
   );
 }
---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="text-light pt-4 text-center">
+      <p>&copy; Todos los derechos reservados.</p>
+      <p>Hecho con ❤️ por Nicolás Herrera</p>
+    </footer>
+  );
+};
+
+export default Footer;
