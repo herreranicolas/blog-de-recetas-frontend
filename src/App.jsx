d698c76718e5ebac3144328edadd56f2effb7761
Nicolas Herrera <herreranicoo17@gmail.com>
2023-06-20 18:30:26 -0300

    Agregue icono boton hamburguesa dispositivos pequeños y modifique tamaño de letra en dispositivos grandes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Menu from "./components/common/Menu";
