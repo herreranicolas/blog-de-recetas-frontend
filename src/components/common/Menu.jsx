d432e28bd3b13edd1f4b7d75e42710b24fb976c3
Nicolas Herrera <herreranicoo17@gmail.com>
2023-06-21 21:29:12 -0300

    Agregue nav links en menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -2,12 +2,13 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { MDBIcon } from "mdb-react-ui-kit";
+import { Link, NavLink } from "react-router-dom";
 
 function Menu() {
   return (
     <Navbar expand="lg" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand as={Link} to="/">
           <span className="me-2">
             <MDBIcon fas icon="utensils" size="lg" />
           </span>
@@ -16,8 +17,12 @@ function Menu() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Iniciar sesión</Nav.Link>
-            <Nav.Link href="#link">Administrador</Nav.Link>
+            <NavLink end className="nav-item nav-link" to="/login">
+              Iniciar sesión
+            </NavLink>
+            <NavLink end className="nav-item nav-link" to="/administrador">
+              Administrador
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
