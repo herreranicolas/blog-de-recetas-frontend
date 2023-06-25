4692171a065145ae217df5882e811a2a26d8c8ba
Nicolas Herrera <herreranicoo17@gmail.com>
2023-06-24 21:33:43 -0300

    Agregue la funcion para eliminar receta

--- src/components/views/recetas/ItemReceta.jsx
+++ src/components/views/recetas/ItemReceta.jsx
@@ -1,6 +1,56 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { borrarReceta } from "../../helpers/queries";
+import Swal from "sweetalert2";
+
 const ItemReceta = ({ receta }) => {
+  const handleDelete = () => {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-outline-danger",
+        cancelButton: "btn btn-danger",
+      },
+      buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons
+      .fire({
+        title: "¿Estas seguro?",
+        text: "¡No podrás revertir este cambio!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Si, borrar receta!",
+        cancelButtonText: "No, cancelar!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          borrarReceta(receta.id).then((respuesta) => {
+            if (respuesta.status === 200) {
+              Swal.fire({
+                title: "¡Receta eliminada!",
+                text: "¡La receta fue eliminada!",
+                icon: "success",
+                confirmButtonColor: "#fa8072",
+              });
+            } else {
+              Swal.fire({
+                title: "Oops! Lo siento!",
+                text: "La receta no pudo ser eliminada.",
+                icon: "error",
+                confirmButtonColor: "#fa8072",
+              });
+            }
+          });
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          swalWithBootstrapButtons.fire(
+            "¡Operación cancelada!",
+            "La receta no fue eliminada",
+            "error"
+          );
+        }
+      });
+  };
   return (
     <tr>
       <td>{receta.nombre}</td>
@@ -24,7 +74,7 @@ const ItemReceta = ({ receta }) => {
           >
             <i className="bi bi-pencil-square"></i>
           </Button>
-          <Button variant="danger">
+          <Button variant="danger" onClick={handleDelete}>
             <i className="bi bi-trash"></i>
           </Button>
         </div>
